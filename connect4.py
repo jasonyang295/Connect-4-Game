c839c396a28de0a4656316fef7f053d9e3df3f5d
Jason Yang <canyoncrestjy@gmail.com>
2021-05-12 23:21:29 -0700

    created a bunch of new functions
    created win condition, checking the board for who won, updating the main function

--- connect4.py
+++ connect4.py
@@ -2,8 +2,28 @@ import numpy as np
 from numpy.lib.function_base import piecewise
 ROW_COUNT = 6
 COL_COUNT = 7
+
+def checkwin(board, piece):
+    	#check all horizontal locations
+		for c in range(COL_COUNT - 3): #we subtract 3 bc the last 3 columns can't have winning horizontal moves
+    			for r in range(ROW_COUNT):
+    					if board[r][c] == piece and board[r][c+1] == piece and board[r][c+2] == piece and board[r][c+3] == piece: #here we check if there are 4 consecutive pieces which = win
+    							return True
+
+
+		#now check vertical locations for win
+
+		for c in range(COL_COUNT): 
+			for r in range(ROW_COUNT - 3): #now we can't have the top rows for vertical win
+				if board[r][c] == piece and board[r+1][c] == piece and board[r+2][c] == piece and board[r+3][c] == piece: #here we check if there are 4 consecutive pieces which = win
+    					return True
+
+		#now we need to check for positively sloped diagonals
+		
+
+		#now we check for negatively sloped diagonal
 def create_board():
-	 board = np.zeros((6,7))
+	 board = np.zeros((ROW_COUNT, COL_COUNT))
 	 return board
 
 def drop_piece(board, row, col, piece):
@@ -31,7 +51,9 @@ while not game_over:
 		if is_valid_location(board, col):
     			row = get_next_open_row(board, col)
 			drop_piece(board, row, col, 1)
-	
+		
+		if checkwin(board, 1):
+    			print("Player1 has won")
 
 	#ask for player 2 input
 	else: 
@@ -39,7 +61,9 @@ while not game_over:
 		if is_valid_location(board, col):
     			row = get_next_open_row(board, col)
 			drop_piece(board, row, col, 2)
-	
+			
+		if checkwin(board, 2):
+    			print("Player2 has won")
 
 
 
